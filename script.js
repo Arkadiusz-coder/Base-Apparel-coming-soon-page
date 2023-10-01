aa0286036b96587c8198bd6a34bff5db3e2b41b4
Arkadiusz <130824900+Arkadiusz-coder@users.noreply.github.com>
2023-10-01 18:05:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const emailInput = document.getElementById("email");
+const resultElement = document.getElementById("result");
+const arrowButton = document.querySelector(".arrow-button");
+
+function validateEmail() {
+  const email = emailInput.value;
+  
+  // Reset button color to its original state
+  arrowButton.style.backgroundImage = "linear-gradient(to right, var(--desaturated-red), var(--soft-red))";
+
+  if (email.match(/^\w+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/)) {
+    resultElement.textContent = "A valid email";
+    resultElement.style.color = "green";
+    document.querySelector(".error-icon").style.display = "none";
+  } else {
+    resultElement.textContent = "Please provide a valid email";
+    resultElement.style.color = "red";
+    document.querySelector(".error-icon").style.display = "block";
+  }
+}
+
+emailInput.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    validateEmail();
+  }
+});
+
+arrowButton.addEventListener("click", function() {
+  arrowButton.classList.toggle("clicked");
+  validateEmail(); // Check email validity when the button is clicked
+});
+
+// Initial button background
+arrowButton.style.backgroundImage = "linear-gradient(to right, var(--desaturated-red), var(--soft-red))";
