20ad458766eae7639a0d897f8e429b7734b398c0
Arkadiusz <130824900+Arkadiusz-coder@users.noreply.github.com>
2023-10-10 18:51:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ function validateEmail() {
   arrowButton.style.backgroundImage = "linear-gradient(to right, var(--desaturated-red), var(--soft-red))";
 
   if (email.match(/^\w+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/)) {
-    resultElement.textContent = "A valid email";
-    resultElement.style.color = "green";
+    resultElement.textContent = "";
     document.querySelector(".error-icon").style.display = "none";
   } else {
     resultElement.textContent = "Please provide a valid email";
